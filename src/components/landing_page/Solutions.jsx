059d7597a6549ed49bc8ee0deea3f7081e5d926d
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-05-08 18:16:35 +0300

    feat: solutions

--- src/components/landing_page/Solutions.jsx
+++ src/components/landing_page/Solutions.jsx
@@ -1,52 +1,49 @@
-const passengerSolutions =[
+const passengerSolutions = [
   {
     id: 1,
-    title: 'Cashless Payments'
+    title: 'Cashless Payments',
   },
   {
     id: 2,
-    title: 'Plan ahead'
+    title: 'Plan ahead',
   },
   {
     id: 3,
-    title: 'Great discounts'
-  }
-]
+    title: 'Great discounts',
+  },
+];
 
 const businessSolutions = [
   {
     id: 1,
-    title: 'Revenue data analysis'
+    title: 'Revenue data analysis',
   },
   {
     id: 2,
-    title: 'Easy management of day to day operations'
+    title: 'Easy management of day to day operations',
   },
-]
-
-
+];
 
 const Solutions = () => {
-
-  console.log(passengerSolutions, businessSolutions)
+  console.log(passengerSolutions, businessSolutions);
 
   return (
-  <div className="min-h-screen flex flex-col gap-16 lg:pt-24 pt-10 lg:px-16 px-8" id="solutions">
-    <h1 className="text-4xl  font-bold text-primary">
-      How we make it easier.
-    </h1>
+    <div
+      className="min-h-screen flex flex-col gap-16 lg:pt-24 pt-10 lg:px-16 px-8"
+      id="solutions"
+    >
+      <h1 className="text-4xl  font-bold text-primary">
+        How we make it easier.
+      </h1>
 
-    <div className="">
-      <h5 className="text-xl text-tertiary font-bold">For passengers</h5>
+      <div className="">
+        <h5 className="text-xl text-tertiary font-bold">For passengers</h5>
+      </div>
 
+      <div className="">
+        <h5 className="text-xl text-tertiary font-bold">For businesses</h5>
+      </div>
     </div>
-
-    <div className="">
-      <h5 className="text-xl text-tertiary font-bold">For businesses</h5>
-
-    </div>
-
-  </div>
   );
 };
 
